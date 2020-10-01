b7dbd8f3767b98867c5ffe637f082cfdac794e77
HP <HP@LAPTOP-QE18AVLA>
2020-10-01 10:36:48 +0530

    Fourth commit

--- ebayAppiumFrmwk/src/test/java/com/test/ebayAppiumFrmwk/TestCase1.java
+++ ebayAppiumFrmwk/src/test/java/com/test/ebayAppiumFrmwk/TestCase1.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.resources.pageObjects.CustomerLoginPage;
 import com.resources.pageObjects.HomePage;
 import com.resources.pageObjects.LoginPage;
@@ -29,57 +31,64 @@ import junit.framework.Assert;
 
 public class TestCase1 extends BaseClass implements Iautoconstant{
 
-	public static void main(String[] args) throws Throwable {
+	@Test
+	public void endToendTestCase() throws Throwable {
 
 		ReusableComponents rc = new ReusableComponents();
-
-		
 		AndroidDriver<AndroidElement> driver=capabilitis(rc.getKeyPropValue(PROP_PATH, "apkFileName"));
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver,30);
 
 		/**
 		 * @author kamlesh
 		 * customer login by using  Phone number and password
 		 * 
+		 * if there is security issue while login then we have to use skip
+		 * signin button
+		 * 
 		 */
+		
 
+/*
 		SignInpage sip = new SignInpage(driver);
 		sip.alreadyCoustomer.click();
 
-		WebDriverWait wait = new WebDriverWait(driver,30);
-	
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText[@index='1']")));
-		
-	
+
+
 		CustomerLoginPage clp = new CustomerLoginPage(driver);
 		clp.loginSearchBox.sendKeys(rc.getKeyPropValue(PROP_PATH, "phoneNumber"));
-		
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.Button[@text='Continue']")));
 
-		
+
 		clp.continuee.click();
 
-		
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText[@text='Amazon password']")));
 		LoginPasswordPage lpp = new LoginPasswordPage(driver);
 		lpp.amazopassword.sendKeys(rc.getKeyPropValue(PROP_PATH, "password"));
 		lpp.signInSUbmit.click();
-		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText[@text='Search']")));
 
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText[@text='Search']")));
+*/
 
 		/**
 		 * 
 		 * @author kamlesh kumar
 		 * customer login by clicking on skip signin Button
 		 * 
+		 * if there is security issue while login then we have to use skip
+		 * signin button
+		 * 
 		 */
 
-		/*     
+		     
 	   LoginPage lp = new LoginPage(driver);
 	   lp.skipSignIn.click();
 
-		 */
+		 
 
 		HomePage hp = new HomePage(driver);
 		hp.searchbox.click();
@@ -91,27 +100,26 @@ public class TestCase1 extends BaseClass implements Iautoconstant{
 
 		((AndroidDriver) driver).pressKey(new KeyEvent(AndroidKey.ENTER));
 
-
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='Samsung 163 cm (65 Inches) Wondertainment Series Ultra HD LED Smart TV UA65TUE60AKXXL (Titan Gray) (2020 model)']")));
 
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Samsung 163 cm (65 Inches) Wondertainment Series Ultra HD LED Smart TV UA65TUE60AKXXL (Titan Gray) (2020 model)']")).click();
 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText[@text='rupees 94,990']")));
-		
+
 		String productPricetext = driver.findElement(By.xpath("//android.widget.EditText[@text='rupees 94,990']")).getText();
-		
+
 		String str="";
 		char[] ch = productPricetext.toCharArray();
 		for (int i = 0; i < ch.length; i++) {
 			if (ch[i]>=48 && ch[i]<=57) {
 				str=str+(char)ch[i];
 			}
-			
+
 		}
 		int productPrice = Integer.parseInt(str);
-		
+
 		System.out.println(productPrice);
-		
+
 		/**
 		 * 
 		 * @author kamlesh
@@ -120,7 +128,7 @@ public class TestCase1 extends BaseClass implements Iautoconstant{
 		 * button.
 		 * 
 		 */
-		
+
 
 		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Add to Cart\"));");
 
@@ -142,32 +150,40 @@ public class TestCase1 extends BaseClass implements Iautoconstant{
 		 * of the user 
 		 * 
 		 */
-		
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.Button[@text='Deliver to this address C-3']")));
 		driver.findElement(By.xpath("//android.widget.Button[@text='Deliver to this address C-3 , central colony']")).click();
-	
-		
-		
+
+
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='94990.00']")));
 
+		/**
+		 * 
+		 * if UI of the application doesnt take you to the Checkout page
+		 * then you need change the xpath of  checkOutproductPricetext and do 
+		 * the assertion accordingly.
+		 * 
+		 */
+
 		String checkOutproductPricetext = driver.findElement(By.xpath("//android.widget.TextView[@text='94990.00']")).getText();
-		
+
 		String str1="";
 		char[] ch1 = checkOutproductPricetext.toCharArray();
 		for (int i = 0; i < ch.length; i++) {
 			if (ch1[i]>=48 && ch1[i]<=57) {
 				str1=str1+(char)ch1[i];
 			}
-			
+
 		}
 		int checkOutproductPrice = Integer.parseInt(str1);
-		
+
 		System.out.println(checkOutproductPrice);
-		
+
 		Assert.assertEquals(productPrice, checkOutproductPrice);
-		
+
 		System.out.println("If productPrice is equal to checkOutproductPrice test case is passed  ");
-		
+
 
 
 	}
